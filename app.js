0251ef6cc173d8b60410fcac2bbfebcb87b8e07e
AmeyVanjare <43752901+AmeyVanjare@users.noreply.github.com>
2020-04-25 12:09:52 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt= require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -18,7 +19,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields: ['password'] });
+
 
 const User = new mongoose.model("User",userSchema);
 
@@ -35,17 +36,20 @@ app.get("/login",function(req,res){
 });
 
 app.post("/register",function(req,res){
-    const newUser= new User({
-        email: req.body.username,
-        password: req.body.password
-    });
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
-        }else{
-            res.render("secrets");
-        }
-    }); 
+
+    bcrypt.hash(req.body.password,saltRounds,function(err,hash){
+        const newUser= new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function(err){
+            if(err){
+                console.log(err);
+            }else{
+                res.render("secrets");
+            }
+        }); 
+    });   
 });
 
 app.post("/login",function(req,res){
@@ -56,9 +60,12 @@ app.post("/login",function(req,res){
             console.log(err);
         }else{
             if(foundUser){
-                if(foundUser.password === password){
-                    res.render("secrets");
-                }
+                bcrypt.compare(password,foundUser.password,function(err,result){
+                    if(result === true){
+                        res.render("secrets");
+                    }
+                });
+               
             }
         }
     });
